1dc59d7f742b4b44eea738af36433774bd4de53b
DreamingLri <3507415455@qq.com>
2024-06-06 20:33:42 +0800

    add actix

---
+++ src/bootstrap/app.rs
@@ -0,0 +1,17 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+pub async fn start() -> std::io::Result<()>{
+
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+
+#[get("/")]
+pub async fn hello() -> impl Responder{
+    HttpResponse::Ok().body("Hello World!")
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+pub mod bootstrap;
+
 #[derive(Clone, Debug)]
 pub struct Link {
     pub line: String,
