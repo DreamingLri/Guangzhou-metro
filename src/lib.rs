8c78168c1d7cceaa7de659b359155adebc08b39a
yescallop <yescallop@gmail.com>
2024-06-06 21:07:25 +0800

    Move code to library

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,14 @@ pub mod bootstrap;
 use std::{
     cmp::Ordering,
     collections::{BinaryHeap, HashMap},
+    fs::File,
+    io::BufReader,
     mem,
+    sync::OnceLock,
 };
 
 use serde::Serialize;
+use serde_json::Value;
 
 struct SubPath<'a> {
     dest: &'a str,
@@ -178,4 +182,63 @@ impl Map {
         }
         Some(Path { segments, len })
     }
+}
+
+pub fn get_map() -> &'static Map {
+    static MAP: OnceLock<Map> = OnceLock::new();
+    MAP.get_or_init(read_map_from_json)
+}
+
+fn read_map_from_json() -> Map {
+    let file = File::open("map.json").expect("failed to open map.json");
+    let rdr = BufReader::new(file);
+    let val: Value = serde_json::from_reader(rdr).expect("failed to parse JSON");
+
+    let Value::Object(obj) = val else {
+        panic!("not an object");
+    };
+
+    let mut map = Map::default();
+
+    for (line, arr) in obj {
+        let Value::Array(arr) = arr else {
+            panic!("not an array");
+        };
+        if arr.len() % 2 != 1 {
+            panic!("even array length");
+        }
+
+        let (Value::String(start), Value::String(end)) = (&arr[0], arr.last().unwrap()) else {
+            panic!("type mismatch");
+        };
+
+        for i in 0..arr.len() / 2 {
+            let [Value::String(fst), Value::Number(cost), Value::String(snd)] =
+                &arr[i * 2..i * 2 + 3]
+            else {
+                panic!("type mismatch");
+            };
+
+            let cost = cost.as_f64().expect("not a f64");
+            map.add_link(
+                fst.clone(),
+                Link {
+                    next: snd.clone(),
+                    cost,
+                    line: line.clone(),
+                    direction: end.clone(),
+                },
+            );
+            map.add_link(
+                snd.clone(),
+                Link {
+                    next: fst.into(),
+                    cost,
+                    line: line.clone(),
+                    direction: start.clone(),
+                },
+            );
+        }
+    }
+    map
 }
