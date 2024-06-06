fee977bb7d75f3f9eef88f70853b7401bbc612d7
DreamingLri <3507415455@qq.com>
2024-06-06 18:07:42 +0800

    add common style

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -8,4 +8,11 @@
 </template>
 
 <style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 </style>
