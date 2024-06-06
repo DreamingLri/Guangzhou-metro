0d2196423a548c20372aa04e09956b5c8df74e03
DreamingLri <3507415455@qq.com>
2024-06-06 18:08:05 +0800

    add element-plus

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
 import App from './App.vue'
 import router from './router'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 createApp(App)
     .use(router)
     .use(Antd)
+    .use(ElementPlus)
     .mount('#app')
