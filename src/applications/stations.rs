22aa5eae1e2a72c290b2c9d63491f85fe4339158
DreamingLri <3507415455@qq.com>
2024-06-07 16:00:34 +0800

    add station func

---
+++ src/applications/stations.rs
@@ -0,0 +1,22 @@
+use actix_web::{get, post, web, HttpResponse, Responder};
+use serde::Deserialize;
+
+
+#[get("/getStation")]
+pub async fn get_station() -> impl Responder {
+    let stations = crate::get_stations();
+    HttpResponse::Ok().json(stations)
+}
+
+#[derive(Deserialize)]
+struct LineRequest {
+    start: String,
+    end: String,
+}
+
+#[post("getLine")]
+pub async fn get_line(info: web::Json<LineRequest>) -> impl Responder {
+    let map = crate::get_map();
+    let path = map.find_path(&info.start, &info.end);
+    HttpResponse::Ok().json(path)
+}
