5c7fbc0d72cd4163585729447c7c71815328f729
DreamingLri <3507415455@qq.com>
2024-06-07 15:59:45 +0800

    add actix-cors

--- src/bootstrap/app.rs
+++ src/bootstrap/app.rs
@@ -1,13 +1,22 @@
 use actix_web::{get, App, HttpServer};
+use actix_cors::Cors;
 
 use crate::applications;
 
 pub async fn start() -> std::io::Result<()> {
-    HttpServer::new(|| 
+    HttpServer::new(|| {
+        let cors = Cors::default()
+            .allow_any_origin()
+            .allow_any_method()
+            .allow_any_header()
+            .max_age(3600);
         App::new()
+        .wrap(cors)
         .service(applications::hello::hello)
         .service(applications::hello::echo)
-    )
+        .service(applications::stations::get_line)
+        .service(applications::stations::get_station)
+        })
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
