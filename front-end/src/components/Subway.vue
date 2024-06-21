ba81f620d9b158c9c9bfcde361e3cdad24c57bd4
DreamingLri <3507415455@qq.com>
2024-06-21 18:47:16 +0800

    add metro map

---
+++ front-end/src/components/Subway.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import {onMounted, ref} from "vue";
+
+const showSessionId = ref('')
+const src = ref('')
+
+onMounted(()=>{
+  showSessionId.value = window.location.search
+  src.value = '../../../public/matro.html'
+})
+</script>
+
+<template>
+  <div style="width: 100%; height: 100vh">
+    <iframe :src="src" frameborder="0" style="height: 100vh; width: 100%"></iframe>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
