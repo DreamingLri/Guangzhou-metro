298b64ee848d4335922f026b692d57c30055b425
DreamingLri <3507415455@qq.com>
2024-06-23 19:40:29 +0800

    add foshan and guangzhou metro map switch

--- front-end/src/components/Subway.vue
+++ front-end/src/components/Subway.vue
@@ -2,17 +2,23 @@
 import {onMounted, ref} from "vue";
 
 const showSessionId = ref('')
-const src = ref('')
+const src_guangzhou = ref('')
+const src_foshan = ref('')
 
 onMounted(()=>{
   showSessionId.value = window.location.search
-  src.value = '../../../public/matro.html'
+  src_guangzhou.value = '../../../public/metro_guangzhou.html'
+  src_foshan.value = '../../../public/metro_foshan.html'
+})
+
+defineProps({
+  map_switch: Boolean
 })
 </script>
 
 <template>
   <div style="width: 100%; height: 100vh">
-    <iframe :src="src" frameborder="0" style="height: 100vh; width: 100%"></iframe>
+    <iframe :src="map_switch ? src_guangzhou : src_foshan" frameborder="0" style="height: 100vh; width: 100%"></iframe>
   </div>
 </template>
 
