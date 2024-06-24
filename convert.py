a56451485855b48adbc8337bafc68b8e6ed8ba86
yescallop <yescallop@gmail.com>
2024-06-24 17:30:31 +0800

    Update convert.py and fix indents in map.json

--- convert.py
+++ convert.py
@@ -2,9 +2,12 @@ import json
 
 obj = json.loads(open("linestation.json", encoding="utf8").read())
 
-out = {}
+out = []
 
 for line in obj["businessObject"]:
-    out[line["lineName"]] = [y for x in line["stations"] for y in (x["stationName"], 0)][:-1]
+    out.append({
+        "name": line["lineName"],
+        "data": [y for x in line["stations"] for y in (x["stationName"], 1)][:-1],
+    })
 
-open("map_init.json", "w", encoding="utf8").write(json.dumps(out, indent=4, ensure_ascii=False))
+open("map_init.json", "w", encoding="utf8").write(json.dumps(out, indent=2, ensure_ascii=False))
